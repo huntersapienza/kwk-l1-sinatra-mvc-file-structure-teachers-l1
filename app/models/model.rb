961474cba03a27d40a88a3d94bbd64372f78874f
Hunter Sapienza <sapienzahunter@gmail.com>
2018-07-18 18:23:41 +0000

    automatically backed up by learn

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,3 +1,8 @@
-class Model
-  # Replace with Dog clas
-end
+
+  # THIS IS WHERE THE RUBY HAPPENDS (yay backend)
+def get_fortune
+  fortunes_array = ["You'll have a great day","You will become a software engineer","You will win the lottery","Jessica might follow you back on instagram..might","Your code will work on the first try","You will have core water for the rest of your life","Maddy and Hunter will teach you for the rest of your life","You will find a puppy","You will find a cat"]
+  return fortunes_array.sample 
+end 
+
+puts get_fortune
